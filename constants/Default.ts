05775d07dd816c37b712e873717b9a2416bfd325
Tani <tannihuang@gmail.com>
2025-02-01 15:42:32 +0800

    update ci-cd

--- constants/Default.ts
+++ constants/Default.ts
@@ -1,13 +1,14 @@
-const BASE_URL_PROD = 'http://ec2-54-168-90-93.ap-northeast-1.compute.amazonaws.com';
+const BASE_URL_PROD = 'https://ec2-54-168-90-93.ap-northeast-1.compute.amazonaws.com/api';
 const BASE_URL_DEV = 'http://localhost:3000';
+const BASE_URL_SOCKET = 'https://ec2-54-168-90-93.ap-northeast-1.compute.amazonaws.com';
 
 const BASE_URL = process.env.NODE_ENV === 'production' ? BASE_URL_PROD : BASE_URL_DEV;
 
 const Default = {
-  signup: `${BASE_URL}/api/auth/signup`,
-  signin: `${BASE_URL}/api/auth/signin`,
-  userByUsername: (username: string) => `${BASE_URL}/api/auth/user/${username}`,
-  chatRoom: `${BASE_URL}/chatroom`,
+  signup: `${BASE_URL}/auth/signup`,
+  signin: `${BASE_URL}/auth/signin`,
+  userByUsername: (username: string) => `${BASE_URL}/auth/user/${username}`,
+  chatRoom: `${BASE_URL_SOCKET}/chatroom`,
 };
 
 export default Default;
