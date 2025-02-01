13ae397ab8be7f81e2289e8b2006004851ffc462
Tani <tannihuang@gmail.com>
2025-02-01 21:53:00 +0800

    add vercel.json

--- constants/Default.ts
+++ constants/Default.ts
@@ -1,6 +1,21 @@
-const BASE_URL_PROD = 'https://ec2-54-168-90-93.ap-northeast-1.compute.amazonaws.com/api';
+// const BASE_URL_PROD = 'https://ec2-54-168-90-93.ap-northeast-1.compute.amazonaws.com/api';
+// const BASE_URL_DEV = 'http://localhost:3000';
+// const BASE_URL_SOCKET = 'https://ec2-54-168-90-93.ap-northeast-1.compute.amazonaws.com';
+
+// const BASE_URL = process.env.NODE_ENV === 'production' ? BASE_URL_PROD : BASE_URL_DEV;
+
+// const Default = {
+//   signup: `${BASE_URL}/auth/signup`,
+//   signin: `${BASE_URL}/auth/signin`,
+//   userByUsername: (username: string) => `${BASE_URL}/auth/user/${username}`,
+//   chatRoom: `${BASE_URL_SOCKET}/chatroom`,
+// };
+
+// export default Default;
+
+const BASE_URL_PROD = 'https://react-native-demo-tau.vercel.app/api';
 const BASE_URL_DEV = 'http://localhost:3000';
-const BASE_URL_SOCKET = 'https://ec2-54-168-90-93.ap-northeast-1.compute.amazonaws.com';
+const BASE_URL_SOCKET = 'https://react-native-demo-tau.vercel.app';
 
 const BASE_URL = process.env.NODE_ENV === 'production' ? BASE_URL_PROD : BASE_URL_DEV;
 
@@ -8,7 +23,8 @@ const Default = {
   signup: `${BASE_URL}/auth/signup`,
   signin: `${BASE_URL}/auth/signin`,
   userByUsername: (username: string) => `${BASE_URL}/auth/user/${username}`,
-  chatRoom: `${BASE_URL_SOCKET}/chatroom`,
+  chatRoom: `${BASE_URL_SOCKET}/chatroom`, // WebSocket 也走 HTTPS
 };
 
 export default Default;
+
