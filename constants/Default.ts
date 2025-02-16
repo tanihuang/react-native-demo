c33e7b870bad8c35ffbaa362157e0d3e70007b3a
Tani <tannihuang@gmail.com>
2025-02-16 14:46:54 +0800

    update url

--- constants/Default.ts
+++ constants/Default.ts
@@ -1,6 +1,6 @@
-// const BASE_URL_PROD = 'https://ec2-54-168-90-93.ap-northeast-1.compute.amazonaws.com/api';
+// const BASE_URL_PROD = 'http://ec2-54-168-90-93.ap-northeast-1.compute.amazonaws.com/api';
 // const BASE_URL_DEV = 'http://localhost:3000';
-// const BASE_URL_SOCKET = 'https://ec2-54-168-90-93.ap-northeast-1.compute.amazonaws.com';
+// const BASE_URL_SOCKET = 'http://ec2-54-168-90-93.ap-northeast-1.compute.amazonaws.com';
 
 // const BASE_URL = process.env.NODE_ENV === 'production' ? BASE_URL_PROD : BASE_URL_DEV;
 
@@ -13,18 +13,19 @@
 
 // export default Default;
 
-const BASE_URL_PROD = 'https://react-native-demo-git-main-tanihuang-project.vercel.app/api';
+const BASE_URL_PROD = 'https://eponovzhw9.execute-api.ap-northeast-1.amazonaws.com/prod/api';
 const BASE_URL_DEV = 'http://localhost:3000';
-const BASE_URL_SOCKET = 'https://react-native-demo-git-main-tanihuang-project.vercel.app';
+const BASE_URL_SOCKET = 'https://eponovzhw9.execute-api.ap-northeast-1.amazonaws.com/prod';
 
 const BASE_URL = process.env.NODE_ENV === 'production' ? BASE_URL_PROD : BASE_URL_DEV;
 
 const Default = {
   signup: `${BASE_URL}/auth/signup`,
   signin: `${BASE_URL}/auth/signin`,
-  userByUsername: (username: string) => `${BASE_URL}/auth/user/${username}`,
+  userByUsername: (username: any) => `${BASE_URL}/auth/user/${username}`,
   chatRoom: `${BASE_URL_SOCKET}/chatroom`, // WebSocket 也走 HTTPS
 };
 
 export default Default;
+
 
